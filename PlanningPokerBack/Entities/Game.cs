6a9a861707f814e175d4be1a795f7edd443bfc23
ChjenNV <ChjenNV@elcomplus.ru>
2022-08-24 16:11:18 +0700

    added primary entities

---
+++ PlanningPokerBack/Entities/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities;
+
+public class Game
+{
+    public string Id { set; get; }
+    
+    public GameStatus Status { set; get; }
+    
+    public List<Player> Players { set; get; }
+}
---
+++ PlanningPokerBack/Entities/Player.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities;
+
+public class Player
+{
+    public string Id { set; get; }
+    
+    public string Nickname { set; get; }
+}
