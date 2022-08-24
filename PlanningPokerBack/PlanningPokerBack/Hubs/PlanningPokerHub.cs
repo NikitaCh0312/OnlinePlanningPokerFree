e4b34c828073ab2b69d38d8f8db75c049912d831
ChjenNV <ChjenNV@elcomplus.ru>
2022-08-24 16:33:05 +0700

    started to create signaR api

--- PlanningPokerBack/PlanningPokerBack/Hubs/PlanningPokerHub.cs
+++ PlanningPokerBack/PlanningPokerBack/Hubs/PlanningPokerHub.cs
@@ -11,13 +11,26 @@ public class PlanningPokerHub: Hub
     {
         _mediator = mediator;
     }
+
+    //adds player to game and broadcasts about new player
+    public async Task add_player_to_game()
+    {
+        
+    }
+
+    //returns state of game to caller
+    public async Task get_game_state()
+    {
+        
+    }
     
     public override async Task OnConnectedAsync()
     {
         await base.OnConnectedAsync();
     }
 
-    public override async Task OnDisconnectedAsync(Exception exception)
+    //removes player from game and broadcsts about player 
+    public override async Task OnDisconnectedAsync(Exception? exception)
     {
         await base.OnDisconnectedAsync(exception);
     }
